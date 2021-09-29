5e7afa16cf01aa88a75c84efa444ecb18268874b
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-09-29 10:52:24 +0530

    Update C1 AA1.py

--- C1 AA1.py
+++ C1 AA1.py
@@ -9,6 +9,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            
     
     pygame.display.update()
